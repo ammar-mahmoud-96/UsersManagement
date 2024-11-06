b2a9946143c87fdb154904829d6d0331e9cc4226
ammar-elbanna <ammarlbanna@gmail.com>
2024-11-06 15:19:27 +0200

    add search input

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./home.module.css";
 import UserItemComponent from "../../components/userItemComponent";
 import useGetUsersList from "../../hooks/useGetUsersList";
 
 export default function Home() {
-  const [usersList, isLoading] = useGetUsersList({});
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredList, setFilteredList] = useState();
+
+  const [usersList, isLoading] = useGetUsersList({
+    onSuccess: (data) => {
+      setFilteredList(data);
+    },
+  });
+
+  const handleSearch = (e) => {
+    const search = e.target.value;
+    setSearchValue(search);
+    const _filterdList = usersList.filter((item) => {
+      return (
+        item.first_name.toLowerCase().includes(search.toLowerCase()) ||
+        item.last_name.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+    setFilteredList(_filterdList);
+  };
   return isLoading ? (
     <div> loading ...</div>
   ) : (
     <div className={styles.container}>
-      {usersList?.map((user) => {
+      <input placeholder="search" value={searchValue} onChange={handleSearch} />
+      {filteredList?.map((user) => {
         return <UserItemComponent key={user.id} user={user} />;
       })}
     </div>
